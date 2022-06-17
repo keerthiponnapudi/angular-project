f3859a7217363292ec6aba8be13600a059fff825
keerthiponnapudi <105340750+keerthiponnapudi@users.noreply.github.com>
2022-06-17 10:14:41 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import {MatSort, Sort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   
+
+  ngOnInit(): void {
+  }
+}
---
+++ app/app.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ToolBoardComponent } from './tool-board/tool-board.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+//import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {DashboardComponent } from './dashboard/dashboard.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { NavigationComponent } from './navigation/navigation.component';
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolBoardComponent,
+    DashboardComponent,
+    NavigationComponent,
+    
+    
+   
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+    MatCardModule
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
